cabe6da6e5c913924de3ca7532910f5588fce992
Bavan Balaji A <124177841+absquare007@users.noreply.github.com>
2026-07-28 11:29:30 +0530

    Time: 2 ms (42.27%) | Memory: 47.4 MB (31.81%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int find(int[] leader, int node){
+        if(leader[node]!=node){
+            leader[node]=find(leader,leader[node]);
+        }
+        return leader[node];
+    }
+    public void joint(int[] leader,int left,int right){
+        leader[find(leader,right)]=find(leader,left);
+    }
+    public int findCircleNum(int[][] isC) {
+        int N=isC.length;
+        int[] leader=new int[N+1];
+        for(int i=0;i<N+1;i++){
+            leader[i]=i;
+        }
+        for(int r=0;r<N;r++){
+            for(int c=0;c<N;c++){
+                if(isC[r][c]==1 && r!=c){
+                    joint(leader,r+1,c+1);
+                }
+            }
+        }
+        int count=0;
+        for(int i=1;i<N+1;i++){
+            if(i==leader[i]) count++;
+        }
+        return count;
+    }
+}
