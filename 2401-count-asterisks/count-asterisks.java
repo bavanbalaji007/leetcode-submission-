c6f4d3319b0e5c748d2d6e31aaa35eaf33c0b5e6
Bavan Balaji A <124177841+bavanbalaji007@users.noreply.github.com>
2026-08-13 22:18:42 +0530

    Time: 1 ms (99.30%) | Memory: 43.1 MB (19.14%) - LeetSync

---
+++ 2401-count-asterisks/count-asterisks.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countAsterisks(String s) {
+        int count = 0;
+        boolean inside = false;
+
+        for (char c : s.toCharArray()) {
+            if (c == '|') {
+                inside = !inside;
+            } else if (c == '*' && !inside) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
