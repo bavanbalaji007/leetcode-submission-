193c25419d4b70fc68670fe84b2855f01bc89ed8
Bavan Balaji A <124177841+bavanbalaji007@users.noreply.github.com>
2026-08-27 22:40:03 +0530

    Time: 2 ms (95.74%) | Memory: 48.1 MB (97.96%) - LeetSync

---
+++ 338-counting-bits/counting-bits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int bcnt(int n){
+        int cnt=0;
+        while(n>0){
+            cnt+=(n&1);
+            n=n>>1;
+        }
+        return cnt;
+    }
+    public int[] countBits(int n) {
+        int[] ans=new int[n+1];
+        for(int i=1;i<=n;i++) ans[i]=bcnt(i);
+        return ans;
+    }
+}
